2152e1ad67f97b4f656a8ba0914b5452ad9a91cd
adva-mo <advamozes@gmail.com>
2023-09-17 23:37:15 +0300

    code refactor

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
 from datetime import datetime
+import time
 
 sunset_url = "https://api.sunrise-sunset.org/json"
 iss_url = "http://api.open-notify.org/iss-now.json"
 MY_LAT = 32.0782587
 MY_LNG = 34.8985302
-
-susnset_params = {"lat": MY_LAT, "lng": MY_LNG, "formatted": 0}
 
 
 def fetch_data(url, params={}):
@@ -15,16 +14,31 @@ def fetch_data(url, params={}):
     return response.json()
 
 
-sun_data = fetch_data(sunset_url, susnset_params)
-sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
-now = datetime.now().hour
-print(sunrise)
-print(sunset)
-print(now)
+def is_iss_overhead():
+    iss_data = fetch_data(iss_url)
+    iss_lat = float(iss_data["iss_position"]["latitude"])
+    iss_lon = float(iss_data["iss_position"]["longitude"])
+    print(iss_lat, iss_lon)
+    return MY_LAT - 5 <= iss_lat <= MY_LAT + 5 and MY_LNG - 5 <= iss_lon <= MY_LNG + 5
 
-iss_data = fetch_data(iss_url)
-iss_lat = float(iss_data["iss_position"]["latitude"])
-iss_lon = float(iss_data["iss_position"]["longitude"])
-print(iss_lat)
-print(iss_lon)
+
+def is_night():
+    susnset_params = {"lat": MY_LAT, "lng": MY_LNG, "formatted": 0}
+    sun_data = fetch_data(sunset_url, susnset_params)
+    sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+    now = datetime.now().hour
+    print(sunrise)
+    print(sunset)
+    print(now)
+    if now >= sunset or now <= sunrise:
+        return True
+
+
+while 1:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        # send email / text message
+        print("iss over head")
+    else:
+        print("not tonight")
