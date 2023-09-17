9844b2a953f358e663fff8a7382577114297efca
adva-mo <advamozes@gmail.com>
2023-09-17 23:02:30 +0300

    fetching iss position

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import requests
 from datetime import datetime
 
 sunset_url = "https://api.sunrise-sunset.org/json"
-iss_irl = "http://api.open-notify.org/iss-now.json"
+iss_url = "http://api.open-notify.org/iss-now.json"
 MY_LAT = 32.0782587
 MY_LNG = 34.8985302
 
 susnset_params = {"lat": MY_LAT, "lng": MY_LNG, "formatted": 0}
 
-response = requests.get(sunset_url, params=susnset_params)
-response.raise_for_status()
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+def fetch_data(url, params={}):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+sun_data = fetch_data(sunset_url, susnset_params)
+sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+now = datetime.now().hour
 print(sunrise)
 print(sunset)
-
-now = datetime.now().hour
 print(now)
+
+iss_data = fetch_data(iss_url)
+iss_lat = float(iss_data["iss_position"]["latitude"])
+iss_lon = float(iss_data["iss_position"]["longitude"])
+print(iss_lat)
+print(iss_lon)
