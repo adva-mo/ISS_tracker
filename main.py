21f013ae5d454286b22bfb4da0263146b71a7002
adva-mo <advamozes@gmail.com>
2023-10-04 15:12:28 +0300

    with sms

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+import time
 import requests
 from datetime import datetime
-import time
+from twilio.rest import Client
+from dotenv import load_dotenv
 
-sunset_url = "https://api.sunrise-sunset.org/json"
-iss_url = "http://api.open-notify.org/iss-now.json"
-MY_LAT = 32.0782587
-MY_LNG = 34.8985302
+load_dotenv()
+
+MY_LAT = float(os.environ.get("MY_LAT"))
+MY_LNG = float(os.environ.get("MY_LNG"))
+
+client = Client(
+    os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN")
+)
 
 
 def fetch_data(url, params={}):
@@ -15,30 +22,32 @@ def fetch_data(url, params={}):
 
 
 def is_iss_overhead():
-    iss_data = fetch_data(iss_url)
+    iss_data = fetch_data(os.environ.get("ISS_URL"))
     iss_lat = float(iss_data["iss_position"]["latitude"])
     iss_lon = float(iss_data["iss_position"]["longitude"])
-    print(iss_lat, iss_lon)
     return MY_LAT - 5 <= iss_lat <= MY_LAT + 5 and MY_LNG - 5 <= iss_lon <= MY_LNG + 5
 
 
 def is_night():
     susnset_params = {"lat": MY_LAT, "lng": MY_LNG, "formatted": 0}
-    sun_data = fetch_data(sunset_url, susnset_params)
+    sun_data = fetch_data(os.environ.get("SUSNSET_URL"), susnset_params)
     sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
     now = datetime.now().hour
-    print(sunrise)
-    print(sunset)
-    print(now)
     if now >= sunset or now <= sunrise:
         return True
+
+
+def sendSMS():
+    message = client.messages.create(
+        to=os.environ.get("MY_NUMBER"),
+        from_=os.environ.get("TWILIO_NUMBER"),
+        body="look up! ISS is overhead",
+    )
+    print(message.sid)
 
 
 while 1:
     time.sleep(60)
     if is_iss_overhead() and is_night():
-        # send email / text message
-        print("iss over head")
-    else:
-        print("not tonight")
+        sendSMS()
